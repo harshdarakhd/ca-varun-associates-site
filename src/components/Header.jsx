327b5dd2355a23e701d1007d2f07aeb75d140b46
Harsh Darak <harshdarak777@gmail.com>
2023-06-02 18:15:56 +0530

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
     <div className='flex justify-center items-center mt-5 space-x-5'>
       <img src={logo} className="w-[100px] h-[70px]" alt="logo" />
       <div>
-        <h1 className='font-normal font-serif text-6xl'>Varun Associates</h1>
-        <h2 className='font-thin font-serif text-2xl ml-2'>Chartered Accountants</h2>
+        <h1 className='font-normal font-serif text-6xl hover:text-blue-500'>Varun Associates</h1>
+        <h2 className='font-thin font-serif text-2xl ml-2 hover:text-blue-500'>Chartered Accountants</h2>
       </div>
     </div>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import Home from '../pages/Home'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='mt-16 ml-16 mb-16 mr-16 inline'>
         <ul className='flex justify-between w-[300px] mx-auto'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
+          {/* {on click of home i want to show the details i will have in my home.js file} */}
+          {/* solution */}
+          {/* <li className='hover:text-blue-500' onClick={Home}>Home</li> */}
+            <li className='hover:text-blue-500' onClick={Home}>Home</li>
+            <li className='hover:text-blue-500'>About</li>
+            <li className='hover:text-blue-500'>Contact</li>
         </ul>
     </div>
   )
