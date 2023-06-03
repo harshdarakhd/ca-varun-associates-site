90bb6212095fff2789e0e2973debe7d15ab6735e
Harsh Darak <harshdarak777@gmail.com>
2023-06-03 18:30:21 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom';
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/AboutUs";
+import Contact from "./pages/ContactUs";
+import Services from "./pages/Services";
 
 
 function App() {
   return (
     <div>
+      <Router>
       <Header/>
-      <Navbar/>
+      {/* <Navbar/> */}
+      <Routes>
+        <Route exact path='/:subpage' element={<Home/>} />
+        {/* <Route exact path='/services' element={<Services/>} />
+        <Route exact path='/about' element={<About/>} />
+        <Route exact path='/contact' element={<Contact/>} /> */}
+      </Routes>
+      {/* <Home/> */}
+      </Router>
     </div>
   );
 }
