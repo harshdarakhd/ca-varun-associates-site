80c69d3e61393a743ca85afad3dbe3fec65f7950
Harsh Darak <harshdarak777@gmail.com>
2023-06-14 17:31:50 +0530

    .

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,66 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 function Navbar() {
-  const location = useLocation();
+    const location = useLocation();
 
-function linkClasses(path) {
-  const isActive = location.pathname === path;
-  return `rounded-full px-4 py-2 gap-1 inline-flex list-none ${
-    isActive ? 'bg-teal-600 text-white' : 'bg-gray-200'
-  } hover:bg-teal-600 hover:text-white`;
-}
+    function linkClasses(path) {
+        const isActive = location.pathname === path;
+        return `rounded-full px-2 py-3 ${
+            isActive ? "bg-teal-600 text-white" : "bg-gray-200"
+        } hover:bg-teal-600 hover:text-white`;
+    }
 
-  return (
-    <div className="nav flex">
-      <ul className="nav flex justify-between w-[550px] mx-auto">
-        <li>
-          <Link to="/" className={linkClasses('/')}>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/services" className={linkClasses('/services')}>
-            Our Services
-          </Link>
-        </li>
-        <li>
-          <Link to="/about" className={linkClasses('/about')}>
-            About Us
-          </Link>
-        </li>
-        <li>
-          <Link to="/contact" className={linkClasses('/contact')}>
-            Contact Us
-          </Link>
-        </li>
-      </ul>
-    </div>
-  );
+    return (
+        // <div className="flex">
+        //   <ul className="flex justify-between w-full mx-auto">
+        //     <li className='sm:text-sm text-center'>
+        //       <Link to="/" className={linkClasses('/')}>
+        //         Home
+        //       </Link>
+        //     </li>
+        //     <li className='sm:text-sm text-center'>
+        //       <Link to="/services" className={linkClasses('/services')}>
+        //         Our Services
+        //       </Link>
+        //     </li>
+        //     <li className='sm:text-sm text-center'>
+        //       <Link to="/about" className={linkClasses('/about')}>
+        //         About Us
+        //       </Link>
+        //     </li>
+        //     <li className='sm:text-sm text-center'>
+        //       <Link to="/contact" className={linkClasses('/contact')}>
+        //         Contact Us
+        //       </Link>
+        //     </li>
+        //   </ul>
+        // </div>
+        <div className="bg-[#D2E9E9] p-5">
+            <ul className="flex justify-center space-x-8">
+                <li className="sm:text-sm text-center">
+                    <Link to="/" className={linkClasses("/")}>
+                        Home
+                    </Link>
+                </li>
+                <li className="sm:text-sm text-center">
+                    <Link to="/services" className={linkClasses("/services")}>
+                        Services
+                    </Link>
+                </li>
+                <li className="sm:text-sm text-center">
+                    <Link to="/about" className={linkClasses("/about")}>
+                        About
+                    </Link>
+                </li>
+                <li className="sm:text-sm text-center">
+                    <Link to="/contact" className={linkClasses("/contact")}>
+                        Contact
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    );
 }
 
 export default Navbar;
